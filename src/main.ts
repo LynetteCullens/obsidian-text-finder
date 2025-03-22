108100bb32230e539395ced6c8c60ce60292e742
LynetteCullens <45722611+LynetteCullens@users.noreply.github.com>
2025-03-22 12:21:38 -0500

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,39 +1,34 @@
-import { App, Plugin, PluginSettingTab, Setting } from "obsidian";
+import { App, Plugin, PluginSettingTab, Setting, MarkdownView } from 'obsidian';
+import * as CodeMirror from "codemirror";
 import { i18n } from "./i18n";
 import { editorExtensionProvider, EditorSearch } from "./editor-extension";
 
+// Combined settings interface
 interface PluginSettings {
-	/**
-	 * 隐藏窗口时清空输入项
-	 */
+	// From Find and Replace in Selection
+	findText: string;
+	findRegexp: string;
+	regexpFlags: string;
+	replace: string;
+
+	// From Text Finder
 	clearAfterHidden: boolean;
-	/**
-	 * 启用输入框的快捷键
-	 */
 	enableInputHotkeys: boolean;
-	/**
-	 * 在搜索启用时,强制进入source模式.在退出后,根据是否开启预览模式切换
-	 */
 	sourceModeWhenSearch: boolean;
-	/**
-	 * 是否移动光标至匹配项
-	 */
 	moveCursorToMatch: boolean;
-	/**
-	 * 是否使用选中文本作为搜索文本
-	 */
 	useSelectionAsSearch: boolean;
-	/**
-	 * 在阅读模式下,命令会调用Obsidian的搜索
-	 */
 	useObsidianSearchInRead: boolean;
-	/**
-	 * 在替换框中支持部分转义字符串:\n \t
-	 */
 	useEscapeCharInReplace: boolean;
 }
 
 const DEFAULT_SETTINGS: PluginSettings = {
+	// From Find and Replace in Selection
+	findText: '',
+	findRegexp: '',
+	regexpFlags: '',
+	replace: '',
+
+	// From Text Finder
 	clearAfterHidden: false,
 	enableInputHotkeys: true,
 	sourceModeWhenSearch: true,
@@ -43,25 +38,82 @@ const DEFAULT_SETTINGS: PluginSettings = {
 	useEscapeCharInReplace: true,
 };
 
-export default class TextFinderPlugin extends Plugin {
-	settings!: PluginSettings;
+export default class CombinedTextFinderPlugin extends Plugin {
+	settings: PluginSettings;
 	editorSearch: EditorSearch | null = null;
+
 	async onload() {
 		await this.loadSettings();
-		this.addSettingTab(new SettingTab(this.app, this));
 
+		// Add commands from Find and Replace in Selection
+		this.addCommand({
+			id: 'find-and-replace-in-selection',
+			name: 'Find and replace in selection',
+			callback: () => this.findAndReplace()
+		});
+
+		// Add settings tab
+		this.addSettingTab(new CombinedSettingTab(this.app, this));
+
+		// Initialize editor search functionality from Text Finder
 		editorExtensionProvider(this);
 	}
+
 	onunload() {
-		// 在取消加载插件的时候销毁finder的svelte组件,不然重复开关会重复创建,虽然没有影响
+		// Clean up editor search component
 		this.editorSearch?.destoryFinder();
 	}
+
+	// From Find and Replace in Selection
+	findAndReplace(): void {
+		let editor = this.getEditor();
+		if (editor) {
+			let selectedText = this.getSelectedText(editor);
+
+			if (this.settings.findText && this.settings.findText != "") {
+				selectedText = selectedText.split(this.settings.findText).join(this.settings.replace);
+			}
+
+			if (this.settings.findRegexp && this.settings.findRegexp != "") {
+				var re = new RegExp(this.settings.findRegexp, this.settings.regexpFlags);
+				selectedText = selectedText.replace(re, this.settings.replace);
+			}
+
+			editor.replaceSelection(selectedText);
+		}
+	}
+
+	getEditor(): CodeMirror.Editor {
+		return this.app.workspace.getActiveViewOfType(MarkdownView)?.sourceMode.cmEditor;
+	}
+
+	getSelectedText(editor: CodeMirror.Editor): string {
+		if (!editor.somethingSelected())
+			this.selectLineUnderCursor(editor);
+
+		return editor.getSelection();
+	}
+
+	selectLineUnderCursor(editor: CodeMirror.Editor) {
+		let selection = this.getLineUnderCursor(editor);
+		editor.getDoc().setSelection(selection.start, selection.end);
+	}
+
+	getLineUnderCursor(editor: CodeMirror.Editor): SelectionRange {
+		let fromCh, toCh: number;
+		let cursor = editor.getCursor();
+
+		fromCh = 0;
+		toCh = editor.getLine(cursor.line).length;
+
+		return {
+			start: { line: cursor.line, ch: fromCh },
+			end: { line: cursor.line, ch: toCh },
+		};
+	}
+
 	async loadSettings() {
-		this.settings = Object.assign(
-			{},
-			DEFAULT_SETTINGS,
-			await this.loadData()
-		);
+		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
 	}
 
 	async saveSettings() {
@@ -69,10 +121,11 @@ export default class TextFinderPlugin extends Plugin {
 	}
 }
 
-class SettingTab extends PluginSettingTab {
-	plugin: TextFinderPlugin;
+// Combined settings tab
+class CombinedSettingTab extends PluginSettingTab {
+	plugin: CombinedTextFinderPlugin;
 
-	constructor(app: App, plugin: TextFinderPlugin) {
+	constructor(app: App, plugin: CombinedTextFinderPlugin) {
 		super(app, plugin);
 		this.plugin = plugin;
 	}
@@ -81,14 +134,63 @@ class SettingTab extends PluginSettingTab {
 		const { containerEl } = this;
 		containerEl.empty();
 
-		const pluginSetting = this.plugin.settings;
+		// Add settings from Find and Replace in Selection
+		containerEl.createEl('h2', { text: 'Find and replace in selection - Settings' });
+
+		new Setting(containerEl)
+			.setName('Text to find')
+			.setDesc('Leave empty to ignore')
+			.addText(text => text
+				.setPlaceholder('')
+				.setValue(this.plugin.settings.findText)
+				.onChange(async (value) => {
+					this.plugin.settings.findText = value;
+					await this.plugin.saveSettings();
+				}));
+
+		new Setting(containerEl)
+			.setName('RegExp to find')
+			.setDesc('Leave empty to ignore')
+			.addText(text => text
+				.setPlaceholder('Example: (\w+)\s(\w+)')
+				.setValue(this.plugin.settings.findRegexp)
+				.onChange(async (value) => {
+					this.plugin.settings.findRegexp = value;
+					await this.plugin.saveSettings();
+				}));
+
+		new Setting(containerEl)
+			.setName('RegExp flags')
+			.setDesc('Type "gmi" for global, multiline, insensitive')
+			.addText(text => text
+				.setPlaceholder('Example: gmi')
+				.setValue(this.plugin.settings.regexpFlags)
+				.onChange(async (value) => {
+					this.plugin.settings.regexpFlags = value;
+					await this.plugin.saveSettings();
+				}));
+
+		new Setting(containerEl)
+			.setName('Replace by')
+			.setDesc('Text to be inserted')
+			.addText(text => text
+				.setPlaceholder('Example: $2, $1')
+				.setValue(this.plugin.settings.replace)
+				.onChange(async (value) => {
+					this.plugin.settings.replace = value;
+					await this.plugin.saveSettings();
+				}));
+
+		// Add settings from Text Finder
+		containerEl.createEl('h2', { text: 'Text Finder - Settings' });
+
 		new Setting(containerEl)
 			.setName(i18n.t("settings.ClearAfterHidden.name"))
 			.setDesc(i18n.t("settings.ClearAfterHidden.desc"))
 			.addToggle((cb) => {
-				cb.setValue(pluginSetting.clearAfterHidden).onChange(
+				cb.setValue(this.plugin.settings.clearAfterHidden).onChange(
 					async (value: boolean) => {
-						pluginSetting.clearAfterHidden = value;
+						this.plugin.settings.clearAfterHidden = value;
 						await this.plugin.saveSettings();
 					}
 				);
@@ -98,9 +200,9 @@ class SettingTab extends PluginSettingTab {
 			.setName(i18n.t("settings.EnableInputHotkeys.name"))
 			.setDesc(i18n.t("settings.EnableInputHotkeys.desc"))
 			.addToggle((cb) => {
-				cb.setValue(pluginSetting.enableInputHotkeys).onChange(
+				cb.setValue(this.plugin.settings.enableInputHotkeys).onChange(
 					async (value: boolean) => {
-						pluginSetting.enableInputHotkeys = value;
+						this.plugin.settings.enableInputHotkeys = value;
 						await this.plugin.saveSettings();
 					}
 				);
@@ -110,9 +212,9 @@ class SettingTab extends PluginSettingTab {
 			.setName(i18n.t("settings.SourceModeWhenSearch.name"))
 			.setDesc(i18n.t("settings.SourceModeWhenSearch.desc"))
 			.addToggle((cb) => {
-				cb.setValue(pluginSetting.sourceModeWhenSearch).onChange(
+				cb.setValue(this.plugin.settings.sourceModeWhenSearch).onChange(
 					async (value: boolean) => {
-						pluginSetting.sourceModeWhenSearch = value;
+						this.plugin.settings.sourceModeWhenSearch = value;
 						await this.plugin.saveSettings();
 					}
 				);
@@ -122,9 +224,9 @@ class SettingTab extends PluginSettingTab {
 			.setName(i18n.t("settings.MoveCursorToMatch.name"))
 			.setDesc(i18n.t("settings.MoveCursorToMatch.desc"))
 			.addToggle((cb) => {
-				cb.setValue(pluginSetting.moveCursorToMatch).onChange(
+				cb.setValue(this.plugin.settings.moveCursorToMatch).onChange(
 					async (value: boolean) => {
-						pluginSetting.moveCursorToMatch = value;
+						this.plugin.settings.moveCursorToMatch = value;
 						await this.plugin.saveSettings();
 					}
 				);
@@ -134,9 +236,9 @@ class SettingTab extends PluginSettingTab {
 			.setName(i18n.t("settings.UseSelectionAsSearch.name"))
 			.setDesc(i18n.t("settings.UseSelectionAsSearch.desc"))
 			.addToggle((cb) => {
-				cb.setValue(pluginSetting.useSelectionAsSearch).onChange(
+				cb.setValue(this.plugin.settings.useSelectionAsSearch).onChange(
 					async (value: boolean) => {
-						pluginSetting.useSelectionAsSearch = value;
+						this.plugin.settings.useSelectionAsSearch = value;
 						await this.plugin.saveSettings();
 					}
 				);
@@ -144,18 +246,11 @@ class SettingTab extends PluginSettingTab {
 
 		new Setting(containerEl)
 			.setName(i18n.t("settings.UseObsidianSearchInRead.name"))
-			.setDesc(
-				i18n.t("settings.UseObsidianSearchInRead.desc", {
-					name:
-						i18n.t("plugin.name") +
-						": " +
-						i18n.t("commands.ShowFind.name"),
-				})
-			)
+			.setDesc(i18n.t("settings.UseObsidianSearchInRead.desc"))
 			.addToggle((cb) => {
-				cb.setValue(pluginSetting.useObsidianSearchInRead).onChange(
+				cb.setValue(this.plugin.settings.useObsidianSearchInRead).onChange(
 					async (value: boolean) => {
-						pluginSetting.useObsidianSearchInRead = value;
+						this.plugin.settings.useObsidianSearchInRead = value;
 						await this.plugin.saveSettings();
 					}
 				);
@@ -165,9 +260,9 @@ class SettingTab extends PluginSettingTab {
 			.setName(i18n.t("settings.UseEscapeCharInReplace.name"))
 			.setDesc(i18n.t("settings.UseEscapeCharInReplace.desc"))
 			.addToggle((cb) => {
-				cb.setValue(pluginSetting.useEscapeCharInReplace).onChange(
+				cb.setValue(this.plugin.settings.useEscapeCharInReplace).onChange(
 					async (value: boolean) => {
-						pluginSetting.useEscapeCharInReplace = value;
+						this.plugin.settings.useEscapeCharInReplace = value;
 						await this.plugin.saveSettings();
 					}
 				);
